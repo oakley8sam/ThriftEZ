7c863dc4abac1ec73b87d62ef6b7e54d907adff4
oakley8sam <oakley8sam@aim.com>
2017-10-05 14:48:20 -0400

    Leg 2 Commit 2

--- app/src/main/java/com/oakley8sam/thriftez/recordActivity.java
+++ app/src/main/java/com/oakley8sam/thriftez/recordActivity.java
@@ -11,6 +11,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class recordActivity extends AppCompatActivity {
 
     @Override
@@ -20,19 +24,26 @@ public class recordActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //////////////////////
+        ArrayList<String> categoryList = new ArrayList<String>();
+
+        categoryList.add("Food");
+        categoryList.add("Rent");
+        //////////////////////
+
         Spinner categorySpinner = (Spinner) findViewById(R.id.categorySpinner);
-        ArrayAdapter categorySpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> categorySpinnerAdapter = new ArrayAdapter<String> (this, R.layout.spinner_item, categoryList);
         categorySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(categorySpinnerAdapter);
 
 
         Spinner monthSpinner = (Spinner) findViewById(R.id.monthField);
-        ArrayAdapter monthSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.months, android.R.layout.simple_spinner_item);
+        ArrayAdapter monthSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.months, R.layout.spinner_item);
         monthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monthSpinner.setAdapter(monthSpinnerAdapter);
 
         Spinner daySpinner = (Spinner) findViewById(R.id.dayField);
-        ArrayAdapter daySpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.febDays, android.R.layout.simple_spinner_item);
+        final ArrayAdapter daySpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.febDays, R.layout.spinner_item);
         daySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         daySpinner.setAdapter(daySpinnerAdapter);
 
@@ -40,6 +51,7 @@ public class recordActivity extends AppCompatActivity {
         daySpinner.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                daySpinnerAdapter.
                 ArrayAdapter daySpinnerAdapter = ArrayAdapter.createFromResource(recordActivity.this, R.array.longDays, android.R.layout.simple_spinner_item);
                 return true;
             }
