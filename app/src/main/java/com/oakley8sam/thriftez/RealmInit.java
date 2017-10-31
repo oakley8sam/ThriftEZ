e11759d79946696f1ab4270b24564932e3cb1848
oakley8sam <oakley8sam@aim.com>
2017-10-31 14:47:11 -0400

    added fabric plugin

--- app/src/main/java/com/oakley8sam/thriftez/RealmInit.java
+++ app/src/main/java/com/oakley8sam/thriftez/RealmInit.java
@@ -2,6 +2,8 @@ package com.oakley8sam.thriftez;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -9,6 +11,7 @@ public class RealmInit extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder().name("ThriftEZRealm.realm").deleteRealmIfMigrationNeeded().build();
         Realm.setDefaultConfiguration(config);
